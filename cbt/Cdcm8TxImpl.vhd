3f01917b2ecbaffe5202e8ecc5dcf477b6632020
Ryotaro Honda <honda@lambda.phys.tohoku.ac.jp>
2024-02-13 12:00:34 +0900

    offset scan

--- cbt/Cdcm8TxImpl.vhd
+++ cbt/Cdcm8TxImpl.vhd
@@ -21,6 +21,9 @@ entity Cdcm8TxImpl is
     dInFromDevice   : in std_logic_vector(kDevW-1 downto 0);
     dOutToPinP      : out std_logic;
     dOutToPinN      : out std_logic;
+    -- Phase Offset --
+    offsetTable     : out SerdesOffsetType;
+    scanFinished    : out std_logic;
     -- Clock and reset
     clkIn           : in std_logic;
     clkDivIn        : in std_logic;
@@ -30,7 +33,9 @@ end Cdcm8TxImpl;
 
 architecture RTL of Cdcm8TxImpl is
   constant kMaxBit  : integer:= 14;
-  signal din_oserdes  : std_logic_vector(kMaxBit-1 downto 0);
+  signal din_oserdes          : std_logic_vector(kMaxBit-1 downto 0);
+  signal reg_din_from_device  : std_logic_vector(dInFromDevice'range);
+  signal din_from_device      : std_logic_vector(dInFromDevice'range);
   signal ocascade_sm_d, ocascade_sm_t : std_logic;
 
   signal  data_out_to_pin   : std_logic;
@@ -41,29 +46,31 @@ architecture RTL of Cdcm8TxImpl is
   signal iserdes_q    : std_logic_vector(13 downto 0);
   signal rx_output    : std_logic_vector(dInFromDevice'range);
 
-  component EdgeDetector is
-    port (
-      rst         : in std_logic;
-      clk         : in std_logic;
-      dIn         : in std_logic;
-      dOut        : out std_logic
-      );
-  end component;
+  signal reg_scan_finished    : std_logic;
+  signal set_additional_delay : std_logic;
+  signal waveform_in          : std_logic_vector(dInFromDevice'range);
+  signal reg_iserdes_out      : std_logic_vector(dInFromDevice'range);
+  signal bit_slip             : std_logic;
 
-  COMPONENT vio_0
-  PORT (
-    clk : IN STD_LOGIC;
-    probe_out0 : OUT STD_LOGIC_VECTOR(0 DOWNTO 0)
-  );
-  END COMPONENT;
+  signal coarse_count         : integer;
+  signal tdc_coarse           : integer;
+  signal tdc_fine             : integer;
+  signal reg_tdc              : SerdesOffsetType;
+  signal ref_index            : integer;
+  signal reg_reftdc           : integer;
+  signal serdes_latency       : integer;
+  signal reg_offset           : SerdesOffsetType;
 
-  signal test_bitslip : std_logic;
-  signal vio_out      : std_logic_vector(0 downto 0);
+  type ScanStateType is (Idle, MeasureTdc, BitSlip, WaitState, CalcOffset, Done);
+  signal state_scan : ScanStateType;
 
   attribute mark_debug  : string;
-  attribute mark_debug of rx_output : signal is "true";
-  attribute mark_debug of din_oserdes : signal is "true";
-  attribute mark_debug of test_bitslip : signal is "true";
+  attribute mark_debug of waveform_in : signal is "true";
+  attribute mark_debug of rx_output   : signal is "true";
+  attribute mark_debug of bit_slip    : signal is "true";
+  attribute mark_debug of state_scan  : signal is "true";
+  attribute mark_debug of reg_tdc     : signal is "true";
+  attribute mark_debug of reg_offset  : signal is "true";
 
 begin
 
@@ -125,9 +132,20 @@ begin
     );
 
 
+  u_extra_delay : process(clkDivIn)
+  begin
+    if(clkDivIn'event and clkDivIn = '1') then
+      if(set_additional_delay = '1') then
+        reg_din_from_device   <= dInFromDevice;
+      end if;
+    end if;
+  end process;
+
+  din_from_device   <= reg_din_from_device when(set_additional_delay = '1') else dInFromDevice;
+
   u_swap : for i in 0 to kDevW-1 generate
   begin
-    din_oserdes(kMaxBit-i-1)     <= dInFromDevice(i);
+    din_oserdes(kMaxBit-i-1)     <= waveform_in(i) when(reg_scan_finished = '0') else din_from_device(i);
     rx_output(i)   <= iserdes_q(i);
   end generate;
   din_oserdes(kMaxBit-kDevW-1 downto 0)  <= (others => '0');
@@ -135,6 +153,9 @@ begin
   -- Feedback ISERDES --------------------------------------------------------
   clk_in      <= clkIn;
   clk_in_inv  <= not clkIn;
+
+  offsetTable   <= reg_offset;
+  scanFinished  <= reg_scan_finished;
 
   u_ISERDESE2_FB : ISERDESE2
     generic map (
@@ -191,13 +212,85 @@ begin
        SHIFTIN2 => '0'
     );
 
-  u_os : EdgeDetector port map('0', clkDivIn, vio_out(0), test_bitslip);
+  u_scan_fasm : process(ioReset, clkDivIn)
+    variable bitslip_count  : integer range -1 to kDevW:=0;
+    variable wait_count     : integer range -1 to 24:=0;
+  begin
+    if(ioReset = '1') then
+      waveform_in           <= X"F0";
+      bitslip_count         := 0;
+      wait_count            := 23;
+      bit_slip              <= '0';
+      reg_scan_finished     <= '0';
+      set_additional_delay  <= '0';
+      state_scan            <= Idle;
+    elsif(clkDivIn'event and clkDivIn = '1') then
+      case state_scan is
+        when Idle =>
+          reg_scan_finished   <= '0';
+          wait_count          := wait_count-1;
+          if(wait_count = 0) then
+            reg_iserdes_out   <= rx_output;
+            waveform_in       <= X"F8";
+            coarse_count      <= 0;
+            state_scan        <= MeasureTdc;
+          end if;
 
-  u_vio : vio_0
-    PORT MAP (
-      clk => clkDivIn,
-      probe_out0 => vio_out
-    );
+        when MeasureTdc =>
+          coarse_count  <= coarse_count +1;
+          if(reg_iserdes_out /= rx_output) then
+            tdc_fine    <= TdcFineCount(rx_output);
+            tdc_coarse  <= coarse_count;
+            state_scan  <= BitSlip;
+          end if;
+
+        when BitSlip =>
+          reg_tdc(bitslip_count)   <= kDevW*tdc_coarse + tdc_fine;
+          if(rx_output = X"F8") then
+            ref_index       <= bitslip_count;
+            reg_reftdc      <= kDevW*tdc_coarse + tdc_fine;
+            serdes_latency  <= tdc_coarse;
+          end if;
+
+          tdc_coarse    <= 0;
+          coarse_count  <= 0;
+          waveform_in   <= X"F0";
+          if(bitslip_count = 7) then
+            state_scan  <= CalcOffset;
+          else
+            bitslip_count := bitslip_count +1;
+            bit_slip    <= '1';
+            wait_count  := 23;
+            state_scan  <= WaitState;
+          end if;
+
+        when WaitState =>
+          bit_slip  <= '0';
+          if(wait_count = 0) then
+            reg_iserdes_out <= rx_output;
+            waveform_in     <= X"F8";
+            state_scan      <= MeasureTdc;
+          end if;
+          wait_count  := wait_count -1;
+
+        when CalcOffset =>
+          for i in 0 to kDevW-1 loop
+            reg_offset(i)   <= reg_tdc(i) - reg_reftdc;
+          end loop;
+          if(serdes_latency /= kRefLatency) then
+            set_additional_delay  <= '1';
+          end if;
+          state_scan  <= Done;
+
+        when Done =>
+          reg_scan_finished   <= '1';
+
+        when others =>
+          state_scan  <= Idle;
+
+      end case;
+    end if;
+  end process;
 
 
 end RTL;
